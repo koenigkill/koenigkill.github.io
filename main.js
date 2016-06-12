17671b53497764a9fbded5d0921667e36c25a758
koenigkill <koenigkill@gmail.com>
2016-06-12 17:58:57 +0200

    Version 1.0

---
+++ main.js
@@ -0,0 +1,17 @@
+var myName = "koenigkill";
+
+var blue = [196, 77, 55];
+var letterColors = [blue];
+
+drawName(myName, letterColors);
+
+if(10 < 3)
+{
+    bubbleShape = 'square';
+}
+else
+{
+    bubbleShape = 'circle';
+}
+
+bounceBubbles();
